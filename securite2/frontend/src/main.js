e4860049778bfaffb1604041a2006898e8d4e89d
Hafida B <ha.boukhari31@gmail.com>
2025-02-18 16:42:46 +0100

    Update main.js

--- securite2/frontend/src/main.js
+++ securite2/frontend/src/main.js
@@ -1,10 +1,24 @@
 import { createApp } from 'vue'
-import './assets/css/style.css'
+//import './assets/css/style.css'
+//import './assets/css/style2.css'
 import router from './router'
+
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 // Components
 import App from './App.vue'
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 createApp(App)
    .use(router)
+   .use(vuetify)
    .mount('#app')
