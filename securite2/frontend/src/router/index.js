93b36e1e89172d01e129891be0fc4684e3ff99a8
Hafida B <ha.boukhari31@gmail.com>
2025-02-18 16:58:48 +0100

    Update index.js

--- securite2/frontend/src/router/index.js
+++ securite2/frontend/src/router/index.js
@@ -1,39 +1,46 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
-import SucessAccount from '/src/views/SucessAccount.vue';
-import Login from '/src/views/Login.vue';
-import VerifyCode from '/src/views/VerifyCode.vue';
-import Visits from '/src/views/Visits.vue';
-import Signup from '/src/views/Signup.vue';
+import Login from '/src/views/Login.vue'
+import Register from '/src/views/Register.vue'
+import Verify_code from '../views/Verify_code.vue';
+import Visit from '../views/Visit.vue';
+import New_visit from '../views/New_visit.vue';
 
 
 
 const routes = [
-   {
-      path: '/',
-      redirect: '/login',
-   },
+    {
+        path: '/',
+        redirect: '/login',
+    },
+
    {
       path: '/login',
       component: Login,
    },
+
    {
-      path: '/verify_code',
-      component: VerifyCode,
+    path: '/register',
+    component: Register,
    },
+
    {
-      path: '/visits',
-      component: Visits,
+    path: '/verify_code',
+    component: Verify_code,
    },
+
    {
-      path: '/signup',
-      component: Signup,
+    path: '/visit',
+    component: Visit,
    },
+
    {
-      path: '/success_account',
-      component: SucessAccount,
-   },
-];
+    path: '/new_visit',
+    component: New_visit,
+     },
+
+ 
+]
 
 const router = createRouter({
    history: createWebHistory('/'),
